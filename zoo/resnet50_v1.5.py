1c83b3077f99c2b9569cb70f9dce113b04cd81b8
andrewferlitsch <aferlitsch@google.com>
2019-08-01 16:51:39 -0700

    better design pattern for ResNet

--- zoo/resnet50_v1.5.py
+++ zoo/resnet50_v1.5.py
@@ -21,6 +21,17 @@ import tensorflow as tf
 from tensorflow.keras import Model
 import tensorflow.keras.layers as layers
 
+def stem(inputs):
+  """ Stem Convolutional Group """
+  # First Convolutional layer, where pooled feature maps will be reduced by 75%
+  x = layers.ZeroPadding2D(padding=(3, 3))(inputs)
+  x = layers.Conv2D(64, kernel_size=(7, 7), strides=(2, 2), padding='valid', use_bias=False, kernel_initializer='he_normal')(x)
+  x = layers.BatchNormalization()(x)
+  x = layers.ReLU()(x)
+  x = layers.ZeroPadding2D(padding=(1, 1))(x)
+  x = layers.MaxPool2D(pool_size=(3, 3), strides=(2, 2))(x)
+  return x
+
 def bottleneck_block(n_filters, x):
     """ Create a Bottleneck Residual Block of Convolutions
         n_filters: number of filters
@@ -42,11 +53,12 @@ def bottleneck_block(n_filters, x):
     x = layers.ReLU()(x)
     return x
 
-def conv_block(n_filters, x, strides=(2,2)):
-    """ Create Block of Convolutions with feature pooling
+def projection_block(n_filters, x, strides=(2,2)):
+    """ Create Bottleneck Residual Block of Convolutions with projection shortcut
         Increase the number of filters by 4X
         n_filters: number of filters
         x        : input into the block
+        strides  : whether the first convolution is strided
     """
     # construct the identity link
     # increase filters by 4X to match shape when added to output of block
@@ -71,51 +83,59 @@ def conv_block(n_filters, x, strides=(2,2)):
     # add the  identity link to the output of the convolution block
     x = layers.add([x, shortcut])
     x = layers.ReLU()(x)
-
     return x
+
+def classifier(x, nclasses):
+    """ The classifier group 
+        x        : input to the classifier
+        nclasses : number of output classes
+    """
+    # Now Pool at the end of all the convolutional residual blocks
+    x = layers.GlobalAveragePooling2D()(x)
+
+    # Final Dense Outputting Layer for 1000 outputs
+    outputs = layers.Dense(nclasses, activation='softmax')(x)
+    return outputs
 
 # The input tensor
 inputs = layers.Input(shape=(224, 224, 3))
 
-# First Convolutional layer, where pooled feature maps will be reduced by 75%
-x = layers.ZeroPadding2D(padding=(3, 3))(inputs)
-x = layers.Conv2D(64, kernel_size=(7, 7), strides=(2, 2), padding='valid', use_bias=False, kernel_initializer='he_normal')(x)
-x = layers.BatchNormalization()(x)
-x = layers.ReLU()(x)
-x = layers.ZeroPadding2D(padding=(1, 1))(x)
-x = layers.MaxPool2D(pool_size=(3, 3), strides=(2, 2))(x)
-
-x = conv_block(64, x, strides=(1,1))
+# The stem convolution group
+x = stem(inputs)
 
 # First Residual Block Group of 64 filters
+# Double the size of filters to fit the next Residual Group
+x = projection_block(64, x, strides=(1,1))
+
+# Identity links
 for _ in range(2):
     x = bottleneck_block(64, x)
 
-# Double the size of filters and reduce feature maps by 75% (strides=2, 2) to fit the next Residual Group
-x = conv_block(128, x)
-
 # Second Residual Block Group of 128 filters
+# Double the size of filters and reduce feature maps by 75% (strides=2, 2) to fit the next Residual Group
+x = projection_block(128, x)
+
+# Identity links
 for _ in range(3):
     x = bottleneck_block(128, x)
 
-# Double the size of filters and reduce feature maps by 75% (strides=2, 2) to fit the next Residual Group
-x = conv_block(256, x)
-
 # Third Residual Block Group of 256 filters
+# Double the size of filters and reduce feature maps by 75% (strides=2, 2) to fit the next Residual Group
+x = projection_block(256, x)
+
+# Identity links
 for _ in range(5):
     x = bottleneck_block(256, x)
 
-# Double the size of filters and reduce feature maps by 75% (strides=2, 2) to fit the next Residual Group
-x = conv_block(512, x)
-
 # Fourth Residual Block Group of 512 filters
+# Double the size of filters and reduce feature maps by 75% (strides=2, 2) to fit the next Residual Group
+x = projection_block(512, x)
+
+# Identity links
 for _ in range(2):
     x = bottleneck_block(512, x)
 
-# Now Pool at the end of all the convolutional residual blocks
-x = layers.GlobalAveragePooling2D()(x)
-
-# Final Dense Outputting Layer for 1000 outputs
-outputs = layers.Dense(1000, activation='softmax')(x)
+# The classifier
+outputs = classifier(x, 1000)
 
 model = Model(inputs, outputs)

53a005fdf2f49430fd7dd867c9b710c963971458
Andrew Ferlitsch <aferlitsch@gmail.com>
2019-08-01 20:33:39 -0700

    design pattern improvements for resnet models

--- zoo/resnet152.py
+++ zoo/resnet152.py
@@ -19,12 +19,34 @@ import tensorflow as tf
 from tensorflow.keras import Model
 import tensorflow.keras.layers as layers
 
+def stem(inputs):
+    """ Create the Stem Convolutional Group
+        inputs : the input vector
+    """
+    # The 224x224 images are zero padded (black - no signal) to be 230x230 images prior to the first convolution
+    x = layers.ZeroPadding2D(padding=(3, 3))(inputs)
+    
+    # First Convolutional layer uses a large (coarse) filter
+    x = layers.Conv2D(64, kernel_size=(7, 7), strides=(2, 2), padding='valid', use_bias=False, kernel_initializer='he_normal')(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.ReLU()(x)
+    
+    # Pooled feature maps will be reduced by 75%
+    x = layers.ZeroPadding2D(padding=(1, 1))(x)
+    x = layers.MaxPool2D(pool_size=(3, 3), strides=(2, 2))(x)
+    return x
+
 def bottleneck_block(n_filters, x):
     """ Create a Bottleneck Residual Block of Convolutions
         n_filters: number of filters
         x        : input into the block
     """
+    
+    # Save input vector (feature maps) for the identity link
     shortcut = x
+    
+    # Construct the 1x1, 3x3, 1x1 residual block
+    
     x = layers.Conv2D(n_filters, (1, 1), strides=(1, 1), use_bias=False, kernel_initializer='he_normal')(x)
     x = layers.BatchNormalization()(x)
     x = layers.ReLU()(x)
@@ -33,27 +55,29 @@ def bottleneck_block(n_filters, x):
     x = layers.BatchNormalization()(x)
     x = layers.ReLU()(x)
 
+    # Increase the number of output filters by 4X
     x = layers.Conv2D(n_filters * 4, (1, 1), strides=(1, 1), use_bias=False, kernel_initializer='he_normal')(x)
     x = layers.BatchNormalization()(x)
 
+    # Add the identity link (input) to the output of the residual block
     x = layers.add([shortcut, x])
     x = layers.ReLU()(x)
     return x
 
-def conv_block(n_filters, x, strides=(2,2)):
+def projection_block(n_filters, x, strides=(2,2)):
     """ Create Block of Convolutions with feature pooling
         Increase the number of filters by 4X
         n_filters: number of filters
         x        : input into the block
     """
-    # construct the identity link
+    # construct the projection shortcut
     # increase filters by 4X to match shape when added to output of block
     shortcut = layers.Conv2D(4 * n_filters, (1, 1), strides=strides, use_bias=False, kernel_initializer='he_normal')(x)
     shortcut = layers.BatchNormalization()(shortcut)
 
     # construct the 1x1, 3x3, 1x1 convolution block
 
-    # feature pooling when strides=(2, 2)
+    # Feature pooling when strides=(2, 2)
     x = layers.Conv2D(n_filters, (1, 1), strides=strides, use_bias=False, kernel_initializer='he_normal')(x)
     x = layers.BatchNormalization()(x)
     x = layers.ReLU()(x)
@@ -62,58 +86,65 @@ def conv_block(n_filters, x, strides=(2,2)):
     x = layers.BatchNormalization()(x)
     x = layers.ReLU()(x)
 
-    # increase the number of filters by 4X
+    # Increase the number of filters by 4X
     x = layers.Conv2D(4 * n_filters, (1, 1), strides=(1, 1), use_bias=False, kernel_initializer='he_normal')(x)
     x = layers.BatchNormalization()(x)
 
-    # add the  identity link to the output of the convolution block
+    # Add the  identity link to the output of the convolution block
     x = layers.add([x, shortcut])
     x = layers.ReLU()(x)
-
     return x
+
+def classifier(x, nclasses):
+    """ Create the Classifier
+        x   : input vector
+        nclasses : number of output classes
+    """
+    # Now Pool at the end of all the convolutional residual blocks
+    x = layers.GlobalAveragePooling2D()(x)
+
+    # Final Dense Outputting Layer for the outputs
+    outputs = layers.Dense(nclasses, activation='softmax')(x)
+    return outputs
 
 # The input tensor
 inputs = layers.Input(shape=(224, 224, 3))
 
-# First Convolutional layer, where pooled feature maps will be reduced by 75%
-x = layers.ZeroPadding2D(padding=(3, 3))(inputs)
-x = layers.Conv2D(64, kernel_size=(7, 7), strides=(2, 2), padding='valid', use_bias=False, kernel_initializer='he_normal')(x)
-x = layers.BatchNormalization()(x)
-x = layers.ReLU()(x)
-x = layers.ZeroPadding2D(padding=(1, 1))(x)
-x = layers.MaxPool2D(pool_size=(3, 3), strides=(2, 2))(x)
-
-x = conv_block(64, x, strides=(1,1))
+# The stem convolutional group
+x = stem(inputs)
 
 # First Residual Block Group of 64 filters
+x = projection_block(64, x, strides=(1,1))
+
+# Identity links blocks
 for _ in range(2):
     x = bottleneck_block(64, x)
 
-# Double the size of filters and reduce feature maps by 75% (strides=2, 2) to fit the next Residual Group
-x = conv_block(128, x)
-
 # Second Residual Block Group of 128 filters
+# Double the size of filters and reduce feature maps by 75% (strides=2, 2) to fit the next Residual Group
+x = projection_block(128, x)
+
+# Identity links blocks
 for _ in range(7):
     x = bottleneck_block(128, x)
 
-# Double the size of filters and reduce feature maps by 75% (strides=2, 2) to fit the next Residual Group
-x = conv_block(256, x)
-
 # Third Residual Block Group of 256 filters
+# Double the size of filters and reduce feature maps by 75% (strides=2, 2) to fit the next Residual Group
+x = projection_block(256, x)
+
+# Identity link blocks
 for _ in range(35):
     x = bottleneck_block(256, x)
 
-# Double the size of filters and reduce feature maps by 75% (strides=2, 2) to fit the next Residual Group
-x = conv_block(512, x)
-
 # Fourth Residual Block Group of 512 filters
+# Double the size of filters and reduce feature maps by 75% (strides=2, 2) to fit the next Residual Group
+x = projection_block(512, x)
+
+# Identity link blocks
 for _ in range(2):
     x = bottleneck_block(512, x)
 
-# Now Pool at the end of all the convolutional residual blocks
-x = layers.GlobalAveragePooling2D()(x)
-
-# Final Dense Outputting Layer for 1000 outputs
-outputs = layers.Dense(1000, activation='softmax')(x)
+# Create the Classifier Group for the 1000 classes
+outputs = classifier(x, 1000)
 
 model = Model(inputs, outputs)

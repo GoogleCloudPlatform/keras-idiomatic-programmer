312dc35b7f9ffcaf54a5eeda87ef39f95a9a73d0
Andrew Ferlitsch <aferlitsch@gmail.com>
2019-08-13 17:51:55 -0700

    update densenet to latest design pattern

--- zoo/densenet/densenet121.py
+++ zoo/densenet/densenet121.py
@@ -22,41 +22,43 @@ def stem(inputs):
     """ The Stem Convolution Group
         inputs : input tensor
     """
-    # First large convolution for abstract features for input 230 x 230 and output
-    # 112 x 112
+    # First large convolution for abstract features for input 230 x 230 and output 112 x 112
     x = layers.Conv2D(64, (7, 7), strides=2)(inputs)
     x = layers.BatchNormalization()(x)
     x = layers.ReLU()(x)
+    
     # Add padding so when downsampling we fit shape 56 x 56
     x = layers.ZeroPadding2D(padding=((1, 1), (1, 1)))(x)
     x = layers.MaxPooling2D((3, 3), strides=2)(x)
     return x
 
-def dense_block(x, nblocks, nb_filters):
+def dense_block(x, n_blocks, n_filters):
     """ Construct a Dense Block
-        x         : input layer
-        nblocks   : number of residual blocks in dense block
-        nb_filters: number of filters in convolution layer in residual block
+        x         : input to the block
+        n_blocks  : number of residual blocks in dense block
+        n_filters : number of filters in convolution layer in residual block
     """
     # Construct a group of residual blocks
-    for _ in range(nblocks):
-        x = residual_block(x, nb_filters)
+    for _ in range(n_blocks):
+        x = residual_block(x, n_filters)
     return x
 
-def residual_block(x, nb_filters):
+def residual_block(x, n_filters):
     """ Construct Residual Block
-        x         : input layer
-        nb_filters: number of filters in convolution layer in residual block
+        x        : input to the block
+        n_filters: number of filters in convolution layer in residual block
     """
-    shortcut = x # remember input tensor into residual block
+    # Remember input tensor into residual block
+    shortcut = x 
 
-    # Bottleneck convolution, expand filters by 4 (DenseNet-B)
-    x = layers.Conv2D(4 * nb_filters, (1, 1), strides=(1, 1))(x)
+    # Dimensionality expansion, expand filters by 4 (DenseNet-B)
+    x = layers.Conv2D(4 * n_filters, (1, 1), strides=(1, 1))(x)
     x = layers.BatchNormalization()(x)
     x = layers.ReLU()(x)
 
-    # 3 x 3 convolution with padding=same to preserve same shape of feature maps
-    x = layers.Conv2D(nb_filters, (3, 3), strides=(1, 1), padding='same')(x)
+    # Bottleneck convolution
+    # 3x3 convolution with padding=same to preserve same shape of feature maps
+    x = layers.Conv2D(n_filters, (3, 3), strides=(1, 1), padding='same')(x)
     x = layers.BatchNormalization()(x)
     x = layers.ReLU()(x)
 
@@ -71,21 +73,31 @@ def trans_block(x, reduce_by):
         reduce_by: percentage of reduction of feature maps
     """
 
-    # Reduce (compression) the number of feature maps (DenseNet-C)
-    # shape[n] returns a class object. We use int() to cast it into the dimension
-    # size
-    nb_filters = int( int(x.shape[3]) * reduce_by )
+    # Reduce (compress) the number of feature maps (DenseNet-C)
+    # shape[n] returns a class object. We use int() to cast it into the dimension size
+    n_filters = int( int(x.shape[3]) * reduce_by )
 
     # Bottleneck convolution
-    x = layers.Conv2D(nb_filters, (1, 1), strides=(1, 1))(x)
+    x = layers.Conv2D(n_filters, (1, 1), strides=(1, 1))(x)
     x = layers.BatchNormalization()(x)
     x = layers.ReLU()(x)
 
-    # Use mean value (average) instead of max value sampling when pooling
-    # reduce by 75%
+    # Use mean value (average) instead of max value sampling when pooling reduce by 75%
     x = layers.AveragePooling2D((2, 2), strides=(2, 2))(x)
     return x
+    
+def classifier(x, n_classes):
+    """ Construct the Classifier Group
+        x         : input to the classifier
+        n_classes : number of output classes
+    """
+    # Global Average Pooling will flatten the 7x7 feature maps into 1D feature maps
+    x = layers.GlobalAveragePooling2D()(x)
+    # Fully connected output layer (classification)
+    x = layers.Dense(n_classes, activation='softmax')(x)
+    return x
 
+# Create the input vector
 inputs = Input(shape=(230, 230, 3))
 
 # Create the Stem Convolution Group
@@ -111,10 +123,8 @@ for nblocks in blocks:
 # Add the last dense block w/o a following transition block
 x = dense_block(x, last, nb_filters)
 
-# Classifier
-# Global Average Pooling will flatten the 7x7 feature maps into 1D feature maps
-x = layers.GlobalAveragePooling2D()(x)
-# Fully connected output layer (classification)
-x = layers.Dense(1000, activation='softmax')(x)
+# Classifier for 1000 classes
+outputs = classifier(x, 1000)
 
-model = Model(inputs, x)
+# Instantiate the model
+model = Model(inputs, outputs)

1a06855d4f7cc1517eff391f5a20be08e6315c74
Andrew Ferlitsch <aferlitsch@gmail.com>
2019-09-08 07:33:25 -0700

    fine tune for pattern updates

--- zoo/densenet/densenet121.py
+++ zoo/densenet/densenet121.py
@@ -12,18 +12,21 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# DenseNet 121 (2016)
+# DenseNet-BC 121 (2016)
 # Paper: https://arxiv.org/pdf/1608.06993.pdf
 
 import tensorflow as tf
 from tensorflow.keras import layers, Input, Model
 
 def stem(inputs):
-    """ The Stem Convolution Group
+    """ Construct the Stem Convolution Group
         inputs : input tensor
     """
-    # First large convolution for abstract features for input 230 x 230 and output 112 x 112
-    x = layers.Conv2D(64, (7, 7), strides=2)(inputs)
+    # Pads input from 224x224 to 230x230
+    x = layers.ZeroPadding2D(padding=((3, 3), (3, 3)))(inputs)
+    
+    # First large convolution for abstract features for input 224 x 224 and output 112 x 112
+    x = layers.Conv2D(64, (7, 7), strides=(2, 2), use_bias=False)(x)
     x = layers.BatchNormalization()(x)
     x = layers.ReLU()(x)
     
@@ -31,8 +34,27 @@ def stem(inputs):
     x = layers.ZeroPadding2D(padding=((1, 1), (1, 1)))(x)
     x = layers.MaxPooling2D((3, 3), strides=2)(x)
     return x
+    
+def learner(x, blocks, n_filters, reduction):
+    """ Construct the Learner
+        x         : input to the learner
+        blocks    : set of number of blocks per group
+        n_filters : number of filters (growth rate)
+        reduction : the amount to reduce (compress) feature maps by
+    """
+    # pop off the list the last dense block
+    last = blocks.pop()
 
-def dense_block(x, n_blocks, n_filters):
+    # Create the dense groups and interceding transition blocks
+    for n_blocks in blocks:
+        x = dense_group(x, n_blocks, n_filters)
+        x = trans_block(x, reduction)
+
+    # Add the last dense block w/o a following transition block
+    x = dense_group(x, last, n_filters)
+    return x
+
+def dense_group(x, n_blocks, n_filters):
     """ Construct a Dense Block
         x         : input to the block
         n_blocks  : number of residual blocks in dense block
@@ -44,43 +66,46 @@ def dense_block(x, n_blocks, n_filters):
     return x
 
 def residual_block(x, n_filters):
-    """ Construct Residual Block
+    """ Construct a Residual Block
         x        : input to the block
         n_filters: number of filters in convolution layer in residual block
     """
     # Remember input tensor into residual block
     shortcut = x 
+    
+    # BN-RE-Conv pre-activation form of convolutions
 
     # Dimensionality expansion, expand filters by 4 (DenseNet-B)
-    x = layers.Conv2D(4 * n_filters, (1, 1), strides=(1, 1))(x)
     x = layers.BatchNormalization()(x)
     x = layers.ReLU()(x)
-
+    x = layers.Conv2D(4 * n_filters, (1, 1), strides=(1, 1), use_bias=False)(x)
+    
     # Bottleneck convolution
     # 3x3 convolution with padding=same to preserve same shape of feature maps
-    x = layers.Conv2D(n_filters, (3, 3), strides=(1, 1), padding='same')(x)
     x = layers.BatchNormalization()(x)
     x = layers.ReLU()(x)
+    x = layers.Conv2D(n_filters, (3, 3), strides=(1, 1), padding='same', use_bias=False)(x)
 
     # Concatenate the input (identity) with the output of the residual block
     # Concatenation (vs. merging) provides Feature Reuse between layers
     x = layers.concatenate([shortcut, x])
     return x
 
-def trans_block(x, reduce_by):
+def trans_block(x, reduction):
     """ Construct a Transition Block
         x        : input layer
-        reduce_by: percentage of reduction of feature maps
+        reduction: percentage of reduction of feature maps
     """
 
     # Reduce (compress) the number of feature maps (DenseNet-C)
     # shape[n] returns a class object. We use int() to cast it into the dimension size
-    n_filters = int( int(x.shape[3]) * reduce_by )
+    n_filters = int( int(x.shape[3]) * reduction)
+    
+    # BN-LI-Conv pre-activation form of convolutions
 
-    # Bottleneck convolution
-    x = layers.Conv2D(n_filters, (1, 1), strides=(1, 1))(x)
     x = layers.BatchNormalization()(x)
-    x = layers.ReLU()(x)
+    # Use 1x1 linear projection convolution
+    x = layers.Conv2D(n_filters, (1, 1), strides=(1, 1), use_bias=False)(x)
 
     # Use mean value (average) instead of max value sampling when pooling reduce by 75%
     x = layers.AveragePooling2D((2, 2), strides=(2, 2))(x)
@@ -96,32 +121,24 @@ def classifier(x, n_classes):
     # Fully connected output layer (classification)
     x = layers.Dense(n_classes, activation='softmax')(x)
     return x
+    
+# Meta-parameter: amount to reduce feature maps by (compression factor) during transition blocks
+reduction = 0.5
 
-# Create the input vector
-inputs = Input(shape=(230, 230, 3))
-
-# Create the Stem Convolution Group
-x = stem(inputs)
+# Meta-parameter: number of filters in a convolution block within a residual block (growth rate)
+n_filters = 32
 
 # number of residual blocks in each dense block
 blocks = [6, 12, 24, 16]
 
-# pop off the list the last dense block
-last   = blocks.pop()
+# The input vector
+inputs = Input(shape=(224, 224, 3))
 
-# amount to reduce feature maps by (compression) during transition blocks
-reduce_by = 0.5
+# The Stem Convolution Group
+x = stem(inputs)
 
-# number of filters in a convolution block within a residual block
-nb_filters = 32
-
-# Create the dense blocks and interceding transition blocks
-for nblocks in blocks:
-    x = dense_block(x, nblocks, nb_filters)
-    x = trans_block(x, reduce_by)
-
-# Add the last dense block w/o a following transition block
-x = dense_block(x, last, nb_filters)
+# The Learner
+x = learner(x, blocks, n_filters, reduction)
 
 # Classifier for 1000 classes
 outputs = classifier(x, 1000)

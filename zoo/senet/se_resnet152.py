4b628e3f5d17f764ae379f2e6b814f5f90541154
Andrew Ferlitsch <aferlitsch@gmail.com>
2019-09-10 20:46:36 -0700

    remove layers

--- zoo/senet/se_resnet152.py
+++ zoo/senet/se_resnet152.py
@@ -17,23 +17,24 @@
 
 import tensorflow as tf
 from tensorflow.keras import Input, Model
-from tensorflow.keras import layers
+from tensorflow.keras.layers import ZeroPadding2D, Conv2D, MaxPooling2D, BatchNormalization, ReLU
+from tensorflow.keras.layers import GlobalAveragePooling2D, Dense, Reshape, Multiply, Add
 
 def stem(inputs):
     """ Construct the Stem Convolutional Group 
         inputs : the input vector
     """
     # The 224x224 images are zero padded (black - no signal) to be 230x230 images prior to the first convolution
-    x = layers.ZeroPadding2D(padding=(3, 3))(inputs)
+    x = ZeroPadding2D(padding=(3, 3))(inputs)
     
     # First Convolutional layer which uses a large (coarse) filter 
-    x = layers.Conv2D(64, (7, 7), strides=(2, 2), padding='valid', use_bias=False, kernel_initializer='he_normal')(x)
-    x = layers.BatchNormalization()(x)
-    x = layers.ReLU()(x)
+    x = Conv2D(64, (7, 7), strides=(2, 2), padding='valid', use_bias=False, kernel_initializer='he_normal')(x)
+    x = BatchNormalization()(x)
+    x = ReLU()(x)
     
     # Pooled feature maps will be reduced by 75%
-    x = layers.ZeroPadding2D(padding=(1, 1))(x)
-    x = layers.MaxPool2D((3, 3), strides=(2, 2))(x)
+    x = ZeroPadding2D(padding=(1, 1))(x)
+    x = MaxPooling2D((3, 3), strides=(2, 2))(x)
     return x
 
 def learner(x, ratio):
@@ -86,20 +87,20 @@ def squeeze_excite_block(x, ratio=16):
 
     # Squeeze (dimensionality reduction)
     # Do global average pooling across the filters, which will the output a 1D vector
-    x = layers.GlobalAveragePooling2D()(x)
+    x = GlobalAveragePooling2D()(x)
     
     # Reshape into 1x1 feature maps (1x1xC)
-    x = layers.Reshape((1, 1, filters))(x)
+    x = Reshape((1, 1, filters))(x)
     
     # Reduce the number of filters (1x1xC/r)
-    x = layers.Dense(filters // ratio, activation='relu', kernel_initializer='he_normal', use_bias=False)(x)
+    x = Dense(filters // ratio, activation='relu', kernel_initializer='he_normal', use_bias=False)(x)
 
     # Excitation (dimensionality restoration)
     # Restore the number of filters (1x1xC)
-    x = layers.Dense(filters, activation='sigmoid', kernel_initializer='he_normal', use_bias=False)(x)
+    x = Dense(filters, activation='sigmoid', kernel_initializer='he_normal', use_bias=False)(x)
 
     # Scale - multiply the squeeze/excitation output with the input (WxHxC)
-    x = layers.multiply([shortcut, x])
+    x = Multiply()([shortcut, x])
     return x
     
 def identity_block(x, n_filters, ratio=16):
@@ -114,25 +115,25 @@ def identity_block(x, n_filters, ratio=16):
     ## Construct the 1x1, 3x3, 1x1 residual block (fig 3c)
 
     # Dimensionality reduction
-    x = layers.Conv2D(n_filters, (1, 1), strides=(1, 1), use_bias=False, kernel_initializer='he_normal')(x)
-    x = layers.BatchNormalization()(x)
-    x = layers.ReLU()(x)
+    x = Conv2D(n_filters, (1, 1), strides=(1, 1), use_bias=False, kernel_initializer='he_normal')(x)
+    x = BatchNormalization()(x)
+    x = ReLU()(x)
 
     # Bottleneck layer
-    x = layers.Conv2D(n_filters, (3, 3), strides=(1, 1), padding="same", use_bias=False, kernel_initializer='he_normal')(x)
-    x = layers.BatchNormalization()(x)
-    x = layers.ReLU()(x)
+    x = Conv2D(n_filters, (3, 3), strides=(1, 1), padding="same", use_bias=False, kernel_initializer='he_normal')(x)
+    x = BatchNormalization()(x)
+    x = ReLU()(x)
 
     # Dimensionality restoration - increase the number of output filters by 4X
-    x = layers.Conv2D(n_filters * 4, (1, 1), strides=(1, 1), use_bias=False, kernel_initializer='he_normal')(x)
-    x = layers.BatchNormalization()(x)
+    x = Conv2D(n_filters * 4, (1, 1), strides=(1, 1), use_bias=False, kernel_initializer='he_normal')(x)
+    x = BatchNormalization()(x)
     
     # Pass the output through the squeeze and excitation block
     x = squeeze_excite_block(x, ratio)
     
     # Add the identity link (input) to the output of the residual block
-    x = layers.add([shortcut, x])
-    x = layers.ReLU()(x)
+    x = Add()([shortcut, x])
+    x = ReLU()(x)
     return x
 
 def projection_block(x, n_filters, strides=(2,2), ratio=16):
@@ -145,32 +146,32 @@ def projection_block(x, n_filters, strides=(2,2), ratio=16):
     """
     # Construct the projection shortcut
     # Increase filters by 4X to match shape when added to output of block
-    shortcut = layers.Conv2D(4 * n_filters, (1, 1), strides=strides, use_bias=False, kernel_initializer='he_normal')(x)
-    shortcut = layers.BatchNormalization()(shortcut)
+    shortcut = Conv2D(4 * n_filters, (1, 1), strides=strides, use_bias=False, kernel_initializer='he_normal')(x)
+    shortcut = BatchNormalization()(shortcut)
 
     ## Construct the 1x1, 3x3, 1x1 residual block (fig 3c)
 
     # Dimensionality reduction
     # Feature pooling when strides=(2, 2)
-    x = layers.Conv2D(n_filters, (1, 1), strides=strides, use_bias=False, kernel_initializer='he_normal')(x)
-    x = layers.BatchNormalization()(x)
-    x = layers.ReLU()(x)
+    x = Conv2D(n_filters, (1, 1), strides=strides, use_bias=False, kernel_initializer='he_normal')(x)
+    x = BatchNormalization()(x)
+    x = ReLU()(x)
 
     # Bottleneck layer
-    x = layers.Conv2D(n_filters, (3, 3), strides=(1, 1), padding='same', use_bias=False, kernel_initializer='he_normal')(x)
-    x = layers.BatchNormalization()(x)
-    x = layers.ReLU()(x)
+    x = Conv2D(n_filters, (3, 3), strides=(1, 1), padding='same', use_bias=False, kernel_initializer='he_normal')(x)
+    x = BatchNormalization()(x)
+    x = ReLU()(x)
 
     # Dimensionality restoration - increase the number of filters by 4X
-    x = layers.Conv2D(4 * n_filters, (1, 1), strides=(1, 1), use_bias=False, kernel_initializer='he_normal')(x)
-    x = layers.BatchNormalization()(x)
+    x = Conv2D(4 * n_filters, (1, 1), strides=(1, 1), use_bias=False, kernel_initializer='he_normal')(x)
+    x = BatchNormalization()(x)
 
     # Pass the output through the squeeze and excitation block
     x = squeeze_excite_block(x, ratio)
 
     # Add the projection shortcut link to the output of the residual block
-    x = layers.add([x, shortcut])
-    x = layers.ReLU()(x)
+    x = Add()([x, shortcut])
+    x = ReLU()(x)
     return x
 
 def classifier(x, n_classes):
@@ -179,10 +180,10 @@ def classifier(x, n_classes):
       n_classes : number of output classes
   """
   # Pool at the end of all the convolutional residual blocks
-  x = layers.GlobalAveragePooling2D()(x)
+  x = GlobalAveragePooling2D()(x)
 
   # Final Dense Outputting Layer for the outputs
-  outputs = layers.Dense(n_classes, activation='softmax')(x)
+  outputs = Dense(n_classes, activation='softmax')(x)
   return outputs
 
 # Meta-parameter: Amount of filter reduction in squeeze operation

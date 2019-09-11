e827c468823d1d7621374e964aa2d90a11ef1552
Andrew Ferlitsch <aferlitsch@gmail.com>
2019-09-10 21:05:15 -0700

    fine tune for latest design pattern

--- zoo/senet/se_resnext101.py
+++ zoo/senet/se_resnext101.py
@@ -16,21 +16,61 @@
 # Paper: https://arxiv.org/pdf/1709.01507.pdf
 
 import tensorflow as tf
-import tensorflow.keras.layers as layers
-from tensorflow.keras import Model
+from tensorflow.keras import Model, Input
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, BatchNormalization, ReLU, Dense, Add
+from tensorflow.keras.layers import Dense, GlobalAveragePooling2D, Reshape, Multiply, Lambda, Concatenate
 
 def stem(inputs):
     """ Construct the Stem Convolution Group
         inputs : input vector
     """
-    x = layers.Conv2D(64, kernel_size=(7, 7), strides=(2, 2), padding='same', kernel_initializer='he_normal')(inputs)
-    x = layers.BatchNormalization()(x)
-    x = layers.ReLU()(x)
-    x = layers.MaxPool2D(pool_size=(3, 3), strides=(2, 2), padding='same')(x)
+    x = Conv2D(64, (7, 7), strides=(2, 2), padding='same', kernel_initializer='he_normal')(inputs)
+    x = BatchNormalization()(x)
+    x = ReLU()(x)
+    x = MaxPooling2D((3, 3), strides=(2, 2), padding='same')(x)
+    return x
+
+def learner(x, ratio):
+    """ Construct the Learner
+	x     : input to the learner
+	ratio : amount of filter reduction during squeeze
+    """
+    # First ResNeXt Group
+    # Double the size of filters to fit the first Residual Group
+    x = se_group(x, 3, 128, 256, ratio=ratio, strides=(1, 1))
+
+    # Second ResNeXt
+    # Double the size of filters and reduce feature maps by 75% (strides=2, 2) to fit the next Residual Group
+    x = se_group(x, 4, 256, 512, ratio=ratio, strides=(1, 1))
+
+    # Third ResNeXt Group
+    # Double the size of filters and reduce feature maps by 75% (strides=2, 2) to fit the next Residual Group
+    x = se_group(x, 23, 512, 1024, ratio=ratio, strides=(1, 1))
+
+    # Fourth ResNeXt Group
+    # Double the size of filters and reduce feature maps by 75% (strides=2, 2) to fit the next Residual Group
+    x = se_group(x, 3, 1024, 2048, ratio=ratio, strides=(1, 1))
+    return x
+
+def se_group(x, n_blocks, filters_in, filters_out, ratio, strides=(2, 2)):
+    """ Construct a Squeeze-Excite Group
+        x          : input to the group
+        n_blocks   : number of blocks in the group
+	filters_in : number of filters  (channels) at the input convolution
+        filters_out: number of filters  (channels) at the output convolution
+	ratio      : amount of filter reduction during squeeze
+        strides    : whether projection block is strided
+    """
+    # First block is a linear projection block
+    x = projection_block(x, filters_in, filters_out, strides=strides, ratio=ratio)
+
+    # Remaining blocks are identity links
+    for _ in range(n_blocks-1):
+        x = identity_block(x, filters_in, filters_out, ratio=ratio) 
     return x
 
 def squeeze_excite_block(x, ratio=16):
-    """ Create a Squeeze and Excite block
+    """ Construct a Squeeze and Excite block
         x    : input to the block
         ratio : amount of filter reduction during squeeze
     """  
@@ -42,23 +82,23 @@ def squeeze_excite_block(x, ratio=16):
 
     # Squeeze (dimensionality reduction)
     # Do global average pooling across the filters, which will the output a 1D vector
-    x = layers.GlobalAveragePooling2D()(x)
+    x = GlobalAveragePooling2D()(x)
     
     # Reshape into 1x1 feature maps (1x1xC)
-    x = layers.Reshape((1, 1, filters))(x)
+    x = Reshape((1, 1, filters))(x)
     
     # Reduce the number of filters (1x1xC/r)
-    x = layers.Dense(filters // ratio, activation='relu', kernel_initializer='he_normal', use_bias=False)(x)
+    x = Dense(filters // ratio, activation='relu', kernel_initializer='he_normal', use_bias=False)(x)
 
     # Excitation (dimensionality restoration)
     # Restore the number of filters (1x1xC)
-    x = layers.Dense(filters, activation='sigmoid', kernel_initializer='he_normal', use_bias=False)(x)
+    x = Dense(filters, activation='sigmoid', kernel_initializer='he_normal', use_bias=False)(x)
 
     # Scale - multiply the squeeze/excitation output with the input (WxHxC)
-    x = layers.multiply([shortcut, x])
+    x = Multiply()([shortcut, x])
     return x
-    
-def bottleneck_block(x, filters_in, filters_out, cardinality=32, ratio=16):
+
+def identity_block(x, filters_in, filters_out, cardinality=32, ratio=16):
     """ Construct a ResNeXT block with identity link
         x          : input to block
         filters_in : number of filters  (channels) at the input convolution
@@ -71,36 +111,36 @@ def bottleneck_block(x, filters_in, filters_out, cardinality=32, ratio=16):
     shortcut = x
 
     # Dimensionality Reduction
-    x = layers.Conv2D(filters_in, kernel_size=(1, 1), strides=(1, 1),
+    x = Conv2D(filters_in, kernel_size=(1, 1), strides=(1, 1),
                       padding='same', kernel_initializer='he_normal')(shortcut)
-    x = layers.BatchNormalization()(x)
-    x = layers.ReLU()(x)
+    x = BatchNormalization()(x)
+    x = ReLU()(x)
 
     # Cardinality (Wide) Layer (split-transform)
     filters_card = filters_in // cardinality
     groups = []
     for i in range(cardinality):
-        group = layers.Lambda(lambda z: z[:, :, :, i * filters_card:i *
+        group = Lambda(lambda z: z[:, :, :, i * filters_card:i *
                               filters_card + filters_card])(x)
-        groups.append(layers.Conv2D(filters_card, kernel_size=(3, 3),
+        groups.append(Conv2D(filters_card, kernel_size=(3, 3),
                                     strides=(1, 1), padding='same', kernel_initializer='he_normal')(group))
 
     # Concatenate the outputs of the cardinality layer together (merge)
-    x = layers.concatenate(groups)
-    x = layers.BatchNormalization()(x)
-    x = layers.ReLU()(x)
+    x = Concatenate()(groups)
+    x = BatchNormalization()(x)
+    x = ReLU()(x)
 
     # Dimensionality restoration
-    x = layers.Conv2D(filters_out, kernel_size=(1, 1), strides=(1, 1),
+    x = Conv2D(filters_out, kernel_size=(1, 1), strides=(1, 1),
                       padding='same', kernel_initializer='he_normal')(x)
-    x = layers.BatchNormalization()(x)
+    x = BatchNormalization()(x)
     
     # Pass the output through the squeeze and excitation block
     x = squeeze_excite_block(x, ratio)
 
     # Identity Link: Add the shortcut (input) to the output of the block
-    x = layers.add([shortcut, x])
-    x = layers.ReLU()(x)
+    x = Add()([shortcut, x])
+    x = ReLU()(x)
     return x
 
 def projection_block(x, filters_in, filters_out, cardinality=32, strides=1, ratio=16):
@@ -115,41 +155,41 @@ def projection_block(x, filters_in, filters_out, cardinality=32, strides=1, rati
     
     # Construct the projection shortcut
     # Increase filters by 2X to match shape when added to output of block
-    shortcut = layers.Conv2D(filters_out, kernel_size=(1, 1), strides=strides,
+    shortcut = Conv2D(filters_out, kernel_size=(1, 1), strides=strides,
                                  padding='same', kernel_initializer='he_normal')(x)
-    shortcut = layers.BatchNormalization()(shortcut)
+    shortcut = BatchNormalization()(shortcut)
 
     # Dimensionality Reduction
-    x = layers.Conv2D(filters_in, kernel_size=(1, 1), strides=(1, 1),
+    x = Conv2D(filters_in, kernel_size=(1, 1), strides=(1, 1),
                       padding='same', kernel_initializer='he_normal')(x)
-    x = layers.BatchNormalization()(x)
-    x = layers.ReLU()(x)
+    x = BatchNormalization()(x)
+    x = ReLU()(x)
 
     # Cardinality (Wide) Layer (split-transform)
     filters_card = filters_in // cardinality
     groups = []
     for i in range(cardinality):
-        group = layers.Lambda(lambda z: z[:, :, :, i * filters_card:i *
+        group = Lambda(lambda z: z[:, :, :, i * filters_card:i *
                               filters_card + filters_card])(x)
-        groups.append(layers.Conv2D(filters_card, kernel_size=(3, 3),
+        groups.append(Conv2D(filters_card, kernel_size=(3, 3),
                                     strides=strides, padding='same', kernel_initializer='he_normal')(group))
 
     # Concatenate the outputs of the cardinality layer together (merge)
-    x = layers.concatenate(groups)
-    x = layers.BatchNormalization()(x)
-    x = layers.ReLU()(x)
+    x = Concatenate()(groups)
+    x = BatchNormalization()(x)
+    x = ReLU()(x)
 
     # Dimensionality restoration
-    x = layers.Conv2D(filters_out, kernel_size=(1, 1), strides=(1, 1),
+    x = Conv2D(filters_out, kernel_size=(1, 1), strides=(1, 1),
                       padding='same', kernel_initializer='he_normal')(x)
-    x = layers.BatchNormalization()(x)
-
+    x = BatchNormalization()(x)
+    
     # Pass the output through the squeeze and excitation block
     x = squeeze_excite_block(x, ratio)
 
     # Add the projection shortcut (input) to the output of the block
-    x = layers.add([shortcut, x])
-    x = layers.ReLU()(x)
+    x = Add()([shortcut, x])
+    x = ReLU()(x)
     return x
     
 def classifier(x, n_classes):
@@ -158,39 +198,21 @@ def classifier(x, n_classes):
         n_classes : number of output classes
     """
     # Final Dense Outputting Layer 
-    x = layers.GlobalAveragePooling2D()(x)
-    outputs = layers.Dense(n_classes, activation='softmax')(x)
+    x = GlobalAveragePooling2D()(x)
+    outputs = Dense(n_classes, activation='softmax')(x)
     return outputs
 
+# Meta-parameter: Amount of filter reduction in squeeze operation
+ratio = 16
+
 # The input tensor
-inputs = layers.Input(shape=(224, 224, 3))
+inputs = Input(shape=(224, 224, 3))
 
 # The Stem Group
 x = stem(inputs)
 
-# First ResNeXt Group
-# Double the size of filters to fit the first Residual Group
-x = projection_block(x, 128, 256, strides=2, ratio=16)
-for _ in range(2):
-    x = bottleneck_block(x, 128, 256, ratio=16)
-
-# Second ResNeXt Group
-# Double the size of filters and reduce feature maps by 75% (strides=2, 2) to fit the next Residual Group
-x = projection_block(x, 256, 512, ratio=16)
-for _ in range(3):
-    x = bottleneck_block(x, 256, 512, ratio=16)
-
-# Third ResNeXt Group
-# Double the size of filters and reduce feature maps by 75% (strides=2, 2) to fit the next Residual Group
-x = projection_block(x, 512, 1024, ratio=16)
-for _ in range(22):
-    x = bottleneck_block(x, 512, 1024, ratio=16)
-
-# Fourth ResNeXt Group
-# Double the size of filters and reduce feature maps by 75% (strides=2, 2) to fit the next Residual Group
-x = projection_block(x, 1024, 2048, ratio=16)
-for _ in range(2):
-    x = bottleneck_block(x, 1024, 2048, ratio=16)
+# The Learner
+x = learner(x, ratio)
 
 # The Classifier for 1000 classes
 outputs = classifier(x, 1000)

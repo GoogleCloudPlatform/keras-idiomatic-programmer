22207a1f42b54d12937ca24ece005ba5f848b4aa
Andrew Ferlitsch <aferlitsch@gmail.com>
2019-09-16 16:08:24 -0700

    composable

---
+++ zoo/resnext/resnext_c.py
@@ -0,0 +1,215 @@
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# ResNeXt (50, 101, 152)
+# Paper: https://arxiv.org/pdf/1611.05431.pdf
+
+import tensorflow as tf
+from tensorflow.keras import Model, Input
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, ReLU, BatchNormalization, Add
+from tensorflow.keras.layers import Concatenate, Dense, GlobalAveragePooling2D, Lambda
+
+class ResNeXt(object):
+    """ Construct a Residual Next Convolution Neural Network """
+    # Meta-parameter: number of filters in, out and number of blocks
+    groups = { 50 : [ (128, 256, 2), (256, 512, 3), (512, 1024, 5), (1024, 2048, 2)],  # ResNeXt 50
+               101: [ (128, 256, 2), (256, 512, 3), (512, 1024, 22), (1024, 2048, 2)], # ResNeXt 101
+               152: [ (128, 256, 2), (256, 512, 7), (512, 1024, 35), (1024, 2048, 2)]  # ResNeXt 152
+             }
+    
+    # Meta-parameter: width of group convolution
+    cardinality = 32
+
+    _model=None
+    init_weights='he_normal'
+
+    def __init__(self, n_layers, cardinality=32, input_shape=(224, 224, 3), n_classes=1000):
+        """ Construct a Residual Next Convolution Neural Network
+            n_layers   : number of layers
+            cardinality: width of group convolution
+            input_shape: the input shape
+            n_classes  : number of output classes
+        """
+        if n_layers not in [50, 101, 152]:
+            raise Exception("ResNeXt: Invalid value for n_layers")
+        
+        # The input tensor
+        inputs = Input(shape=input_shape)
+
+        # The Stem Group
+        x = self.stem(inputs)
+
+        # The Learner
+        x = self.learner(x, self.groups[n_layers], cardinality)
+
+        # The Classifier for 1000 classes
+        outputs = self.classifier(x, n_classes)
+
+        # Instantiate the Model
+        self._model = Model(inputs, outputs)
+
+    @property
+    def model(self):
+        return self._model
+
+    @model.setter
+    def model(self, _model):
+        self._model = _model
+
+    def stem(self, inputs):
+        """ Construct the Stem Convolution Group
+            inputs : input vector
+        """
+        x = Conv2D(64, (7, 7), strides=(2, 2), padding='same', kernel_initializer=self.init_weights, use_bias=False)(inputs)
+        x = BatchNormalization()(x)
+        x = ReLU()(x)
+        x = MaxPooling2D(pool_size=(3, 3), strides=(2, 2), padding='same')(x)
+        return x
+    
+    def learner(self, x, groups, cardinality=32):
+        """ Construct the Learner
+            x          : input to the learner
+            groups     : list of groups: filters in, filters out, number of blocks
+            cardinality: width of group convolution
+        """
+        # First ResNeXt Group (not-strided)
+        filters_in, filters_out, n_blocks = groups.pop(0)
+        x = ResNeXt.group(x, filters_in, filters_out, n_blocks, strides=(1, 1), cardinality=cardinality)
+
+        # Remaining ResNeXt groups
+        for filters_in, filters_out, n_blocks in groups:
+            x = ResNeXt.group(x, filters_in, filters_out, n_blocks, cardinality=cardinality)
+        return x
+
+    @staticmethod
+    def group(x, filters_in, filters_out, n_blocks, cardinality=32, strides=(2, 2), init_weights=None):
+        """ Construct a Residual group
+            x          : input to the group
+            filters_in : number of filters  (channels) at the input convolution
+            filters_out: number of filters (channels) at the output convolution
+            cardinality: width of group convolution
+            strides    : whether its a strided convolution
+        """
+        # Double the size of filters to fit the first Residual Group
+        # Reduce feature maps by 75% (strides=2, 2) to fit the next Residual Group
+        x = ResNeXt.projection_block(x, filters_in, filters_out, strides=strides, cardinality=cardinality, init_weights=init_weights)
+
+        # Remaining blocks
+        for _ in range(n_blocks):
+            x = ResNeXt.identity_block(x, filters_in, filters_out, cardinality=cardinality, init_weights=init_weights)	
+        return x
+
+    @staticmethod
+    def identity_block(x, filters_in, filters_out, cardinality=32, init_weights=None):
+        """ Construct a ResNeXT block with identity link
+            x          : input to block
+            filters_in : number of filters  (channels) at the input convolution
+            filters_out: number of filters (channels) at the output convolution
+            cardinality: width of group convolution
+        """
+        if init_weights is None:
+            init_weights = ResNeXt.init_weights
+            
+        # Remember the input
+        shortcut = x
+
+        # Dimensionality Reduction
+        x = Conv2D(filters_in, (1, 1), strides=(1, 1),
+                   padding='same', kernel_initializer=init_weights, use_bias=False)(shortcut)
+        x = BatchNormalization()(x)
+        x = ReLU()(x)
+
+        # Cardinality (Wide) Layer (split-transform)
+        filters_card = filters_in // cardinality
+        groups = []
+        for i in range(cardinality):
+            group = Lambda(lambda z: z[:, :, :, i * filters_card:i * filters_card + filters_card])(x)
+            groups.append(Conv2D(filters_card, (3, 3), strides=(1, 1),
+                                 padding='same', kernel_initializer=init_weights, use_bias=False)(group))
+
+        # Concatenate the outputs of the cardinality layer together (merge)
+        x = Concatenate()(groups)
+        x = BatchNormalization()(x)
+        x = ReLU()(x)
+
+        # Dimensionality restoration
+        x = Conv2D(filters_out, (1, 1), strides=(1, 1),
+                   padding='same', kernel_initializer=init_weights, use_bias=False)(x)
+        x = BatchNormalization()(x)
+
+        # Identity Link: Add the shortcut (input) to the output of the block
+        x = Add()([shortcut, x])
+        x = ReLU()(x)
+        return x
+
+    @staticmethod
+    def projection_block(x, filters_in, filters_out, cardinality=32, strides=(2, 2), init_weights=None):
+        """ Construct a ResNeXT block with projection shortcut
+            x          : input to the block
+            filters_in : number of filters  (channels) at the input convolution
+            filters_out: number of filters (channels) at the output convolution
+            cardinality: width of group convolution
+            strides    : whether entry convolution is strided (i.e., (2, 2) vs (1, 1))
+        """
+        if init_weights is None:
+            init_weights = ResNeXt.init_weights
+    
+        # Construct the projection shortcut
+        # Increase filters by 2X to match shape when added to output of block
+        shortcut = Conv2D(filters_out, (1, 1), strides=strides,
+                          padding='same', kernel_initializer=init_weights)(x)
+        shortcut = BatchNormalization()(shortcut)
+
+        # Dimensionality Reduction
+        x = Conv2D(filters_in, (1, 1), strides=(1, 1),
+                   padding='same', kernel_initializer=init_weights, use_bias=False)(x)
+        x = BatchNormalization()(x)
+        x = ReLU()(x)
+
+        # Cardinality (Wide) Layer (split-transform)
+        filters_card = filters_in // cardinality
+        groups = []
+        for i in range(cardinality):
+            group = Lambda(lambda z: z[:, :, :, i * filters_card:i * filters_card + filters_card])(x)
+            groups.append(Conv2D(filters_card, (3, 3), strides=strides,
+                                 padding='same', kernel_initializer=init_weights, use_bias=False)(group))
+
+        # Concatenate the outputs of the cardinality layer together (merge)
+        x = Concatenate()(groups)
+        x = BatchNormalization()(x)
+        x = ReLU()(x)
+
+        # Dimensionality restoration
+        x = Conv2D(filters_out, (1, 1), strides=(1, 1),
+                   padding='same', kernel_initializer=init_weights, use_bias=False)(x)
+        x = BatchNormalization()(x)
+
+        # Identity Link: Add the shortcut (input) to the output of the block
+        x = Add()([shortcut, x])
+        x = ReLU()(x)
+        return x
+    
+    def classifier(self, x, n_classes):
+        """ Construct the Classifier
+            x         : input to the classifier
+            n_classes : number of output classes
+        """
+        # Final Dense Outputting Layer 
+        x = GlobalAveragePooling2D()(x)
+        outputs = Dense(n_classes, activation='softmax')(x)
+        return outputs
+
+
+# Example
+# resnext = ResNeXt(50)

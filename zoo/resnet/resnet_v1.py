6565e574c1ee7226180fe3fa0ba0aac130cf3dcc
Andrew Ferlitsch <aferlitsch@gmail.com>
2019-09-15 18:20:13 -0700

    make corrections to comment and key

--- zoo/resnet/resnet_v1.py
+++ zoo/resnet/resnet_v1.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# ResNet50 v1.0
+# ResNet (50, 101, 152): v1.0
 # Paper: https://arxiv.org/pdf/1512.03385.pdf
 
 import tensorflow as tf
@@ -142,9 +142,9 @@ def classifier(x, n_classes):
   return outputs
 
 # Meta-parameter: list of groups: filter size and number of blocks
-groups = { '50' : [ (64, 2), (128, 3), (256, 5),  (512, 2) ],		# ResNet50
-           '101': [ (64, 2), (128, 3), (256, 22), (512, 2) ],		# ResNet101
-           '101': [ (64, 2), (128, 7), (256, 35), (512, 2) ]		# ResNet152
+groups = { 50 : [ (64, 2), (128, 3), (256, 5),  (512, 2) ],		# ResNet50
+           101: [ (64, 2), (128, 3), (256, 22), (512, 2) ],		# ResNet101
+           152: [ (64, 2), (128, 7), (256, 35), (512, 2) ]		# ResNet152
          }
 
 # The input tensor
@@ -154,7 +154,7 @@ inputs = Input(shape=(224, 224, 3))
 x = stem(inputs)
 
 # The learner
-x = learner(x, groups['50'])
+x = learner(x, groups[50])
 
 # The classifier for 1000 classes
 outputs = classifier(x, 1000)

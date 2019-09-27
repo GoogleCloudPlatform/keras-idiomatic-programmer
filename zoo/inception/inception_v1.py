a903fd3bb7a4b5d605feb6d3cdc9aac4cedc0449
Andrew Ferlitsch <aferlitsch@gmail.com>
2019-09-27 16:18:00 -0700

    Add files via upload

--- zoo/inception/inception_v1.py
+++ zoo/inception/inception_v1.py
@@ -18,7 +18,7 @@
 import tensorflow as tf
 from tensorflow.keras import Model, Input
 from tensorflow.keras.layers import Conv2D, ReLU, ZeroPadding2D
-from tensorflow.keras.layers import MaxPooling2D, Dense, Add, GlobalAveragePooling2D
+from tensorflow.keras.layers import MaxPooling2D, Dense, Add, AveragePooling2D
 
 def stem(inputs):
     """ Construct the Stem Convolutional Group 
@@ -28,7 +28,15 @@ def stem(inputs):
     x = ZeroPadding2D(padding=(3, 3))(inputs)
     
     # First Convolutional layer which uses a large (coarse) filter 
-    x = Conv2D(64, (7, 7), strides=(2, 2), padding='valid', use_bias=False, kernel_initializer='he_normal')(x)
+    x = Conv2D(64, (7, 7), strides=(2, 2), padding='valid', use_bias=False, kernel_initializer='glorot_uniform')(x)
+    x = ReLU()(x)
+    
+    # Pooled feature maps will be reduced by 75%
+    x = ZeroPadding2D(padding=(1, 1))(x)
+    x = MaxPooling2D((3, 3), strides=(2, 2))(x)
+
+    # Second Convolutional layer which uses a mid-size filter 
+    x = Conv2D(64, (3, 3), strides=(2, 2), padding='valid', use_bias=False, kernel_initializer='glorot_uniform')(x)
     x = ReLU()(x)
     
     # Pooled feature maps will be reduced by 75%
@@ -41,40 +49,83 @@ def learner(x, n_classes):
         x        : input to the learner
         n_classes: number of output classes
     """
-    # Dimensiionality Expansion Groups
-    x = group(x, 3, 64)
-    x = group(x, 1, 128)
-    # Auxiliary Classifier
-    x = auxiliary(x, n_classes) 
-    x = group(x, 2, 192)
+    # Group 3
+    x = group(x, [((64,),  (96,128),   (16, 32), (32,)),  # 3a
+                  ((128,), (128, 192), (32, 96), (64,))]) # 3b
 
-    # Dimensionality Reduction Groups
-    x = group(x, 1, 160)
-    # Auxiliary Classifier
-    x = auxiliary(x, n_classes)
-    x = group(x, 2, 128)
+    # Group 4
+    x = group(x, [((192,),  (96, 208), (16, 48), (64,)), # 4a
+                  None, 				 # auxiliary classifier
+                  ((160,), (112, 224), (24, 64), (64,)), # 4b
+                  ((128,), (128, 256), (24, 64), (64,)), # 4c
+                  ((112,), (144, 288), (32, 64), (64,)), # 4d
+                  None,                                  # auxiliary classifier
+                  ((256,), (160, 320), (32, 128), (128,))], # 4e
+                  n_classes=n_classes) 
+
+    # Group 5
+    x = group(x, [((256,), (160, 320), (32, 128), (128,)), # 5a
+                  ((384,), (192, 384), (48, 128), (128,))],# 5b
+                 pooling=False) 
     return x
 
-def group(x)
-    """ Construct a Residual Group 
+def group(x, blocks, pooling=True, n_classes=1000):
+    """ Construct an Inception group
         x         : input into the group
+        blocks    : filters for each block in the group
+        pooling   : whether to end the group with max pooling
+        n_classes : number of classes for auxiliary classifier
     """
+    # Construct the inception blocks (modules)
+    for block in blocks:
+        # Add auxiliary classifier
+        if block is None:
+            auxiliary(x, n_classes)
+        else:
+            x = inception_block(x, block[0], block[1], block[2], block[3])           
+
+    if pooling:
+        x = MaxPooling2D((3, 3), strides=2)(x)
     return x
+
+def inception_block(x, f1x1, f3x3, f5x5, fpool):
+    """ Construct an Inception block (module)
+        x    : input to the block
+        f1x1 : filters for 1x1 branch
+        f3x3 : filters for 3x3 branch
+        f5x5 : filters for 5x5 branch
+        fpool: filters for pooling branch
+    """
+    # 1x1 branch
+    b1x1 = Conv2D(f1x1[0], (1, 1), strides=1, padding='same', activation='relu', kernel_initializer='glorot_uniform')(x)
+
+    # 3x3 branch
+    b3x3 = Conv2D(f3x3[0], (1, 1), strides=1, padding='same', activation='relu', kernel_initializer='glorot_uniform')(x)
+    b3x3 = ZeroPadding2D((1,1))(b3x3)
+    b3x3 = Conv2D(f3x3[1], (3, 3), strides=1, padding='valid', activation='relu', kernel_initializer='glorot_uniform')(b3x3)
+    return x
+
+def auxiliary(x, n_classes):
+    """ Construct the auxiliary classier
+        x        : input to the auxiliary classifier
+        n_classes: number of output classes
+    """
+    pass
 
 def classifier(x, n_classes, dropout=0.4):
-  """ Construct the Classifier Group 
-      x         : input to the classifier
-      n_classes : number of output classes
-      dropout   : percentage for dropout rate
-  """
-  # Pool at the end of all the convolutional residual blocks
-  x = AveragePooling2D((7, 7))(x)
-  x = Flatten()(x)
-  x = Dropout(dropout)
+    """ Construct the Classifier Group 
+        x         : input to the classifier
+        n_classes : number of output classes
+        dropout   : percentage for dropout rate
+    """
+    # Pool at the end of all the convolutional residual blocks
+    x = AveragePooling2D((7, 7))(x)
+    x = Flatten()(x)
+    x = Dropout(dropout)
 
-  # Final Dense Outputting Layer for the outputs
-  outputs = Dense(n_classes, activation='softmax', kernel_initializer='glorot_uniform')(x)
-  return outputs
+    # Final Dense Outputting Layer for the outputs
+    outputs = Dense(n_classes, activation='softmax', kernel_initializer='glorot_uniform')(x)
+    return outputs
 
 # Meta-parameter: dropout percentage
 dropout = 0.4
@@ -86,7 +137,7 @@ inputs = Input(shape=(224, 224, 3))
 x = stem(inputs)
 
 # The learner
-x = learner(x)
+x = learner(x, 1000)
 
 # The classifier for 1000 classes
 outputs = classifier(x, 1000, dropout)

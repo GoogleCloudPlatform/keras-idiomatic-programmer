7f8493ec385cfc8dcf41dd28d9b87e45f7d4df33
Andrew Ferlitsch <aferlitsch@gmail.com>
2019-09-27 18:38:24 -0700

    Add files via upload

--- zoo/inception/inception_v1.py
+++ zoo/inception/inception_v1.py
@@ -49,12 +49,15 @@ def learner(x, n_classes):
         x        : input to the learner
         n_classes: number of output classes
     """
+    aux = [] # Auxiliary Outputs
+
     # Group 3
-    x, _ = group(x, [((64,),  (96,128),   (16, 32), (32,)),  # 3a
+    x, o = group(x, [((64,),  (96,128),   (16, 32), (32,)),  # 3a
                      ((128,), (128, 192), (32, 96), (64,))]) # 3b
+    aux += o
 
     # Group 4
-    x, _ = group(x, [((192,),  (96, 208), (16, 48), (64,)), # 4a
+    x, o = group(x, [((192,),  (96, 208), (16, 48), (64,)), # 4a
                      None, 				 # auxiliary classifier
                      ((160,), (112, 224), (24, 64), (64,)), # 4b
                      ((128,), (128, 256), (24, 64), (64,)), # 4c
@@ -62,12 +65,14 @@ def learner(x, n_classes):
                      None,                                  # auxiliary classifier
                      ((256,), (160, 320), (32, 128), (128,))], # 4e
                      n_classes=n_classes) 
+    aux += o
 
     # Group 5
-    x, _ = group(x, [((256,), (160, 320), (32, 128), (128,)), # 5a
+    x, o = group(x, [((256,), (160, 320), (32, 128), (128,)), # 5a
                      ((384,), (192, 384), (48, 128), (128,))],# 5b
                      pooling=False) 
-    return x
+    aux += o
+    return x, aux
 
 def group(x, blocks, pooling=True, n_classes=1000):
     """ Construct an Inception group
@@ -76,20 +81,20 @@ def group(x, blocks, pooling=True, n_classes=1000):
         pooling   : whether to end the group with max pooling
         n_classes : number of classes for auxiliary classifier
     """
-    outputs = [] # Auxiliary Outputs
+    aux = [] # Auxiliary Outputs
 
     # Construct the inception blocks (modules)
     for block in blocks:
         # Add auxiliary classifier
         if block is None:
-            outputs.append(auxiliary(x, n_classes))
+           aux.append(auxiliary(x, n_classes))
         else:
             x = inception_block(x, block[0], block[1], block[2], block[3])           
 
     if pooling:
         x = ZeroPadding2D(padding=(1, 1))(x)
         x = MaxPooling2D((3, 3), strides=2)(x)
-    return x, outputs
+    return x, aux
 
 def inception_block(x, f1x1, f3x3, f5x5, fpool):
     """ Construct an Inception block (module)
@@ -103,17 +108,20 @@ def inception_block(x, f1x1, f3x3, f5x5, fpool):
     b1x1 = Conv2D(f1x1[0], (1, 1), strides=1, padding='same', activation='relu', kernel_initializer='glorot_uniform')(x)
 
     # 3x3 branch
+    # 1x1 reduction
     b3x3 = Conv2D(f3x3[0], (1, 1), strides=1, padding='same', activation='relu', kernel_initializer='glorot_uniform')(x)
     b3x3 = ZeroPadding2D((1,1))(b3x3)
     b3x3 = Conv2D(f3x3[1], (3, 3), strides=1, padding='valid', activation='relu', kernel_initializer='glorot_uniform')(b3x3)
 
     # 5x5 branch
+    # 1x1 reduction
     b5x5 = Conv2D(f5x5[0], (1, 1), strides=1, padding='same', activation='relu', kernel_initializer='glorot_uniform')(x)
     b5x5 = ZeroPadding2D((1,1))(b5x5)
     b5x5 = Conv2D(f5x5[1], (3, 3), strides=1, padding='valid', activation='relu', kernel_initializer='glorot_uniform')(b5x5)
 
     # Pooling branch
     bpool = MaxPooling2D((3, 3), strides=1, padding='same')(x)
+    # 1x1 projection
     bpool = Conv2D(fpool[0], (1, 1), strides=1, padding='same', activation='relu', kernel_initializer='glorot_uniform')(bpool)
 
     # Concatenate the outputs (filters) of the branches
@@ -158,10 +166,10 @@ inputs = Input(shape=(224, 224, 3))
 x = stem(inputs)
 
 # The learner
-x = learner(x, 1000)
+x, aux = learner(x, 1000)
 
 # The classifier for 1000 classes
 outputs = classifier(x, 1000, dropout)
 
 # Instantiate the Model
-model = Model(inputs, outputs)
+model = Model(inputs, [outputs] + aux)

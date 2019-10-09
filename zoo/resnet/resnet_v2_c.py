358557aca95a24ecf40259b780e625d71820de0a
Andrew Ferlitsch <aferlitsch@google.com>
2019-10-08 19:10:46 -0700

    fix comment

--- zoo/resnet/resnet_v2_c.py
+++ zoo/resnet/resnet_v2_c.py
@@ -26,7 +26,7 @@ from tensorflow.keras.layers import Dense, GlobalAveragePooling2D, Add
 
 class ResNetV2(object):
     """ Construct a Residual Convolution Network Network V2 """
-    # Meta-parameter: list of groups: filter size and number of blocks
+    # Meta-parameter: list of groups: number of filters and number of blocks
     groups = { 50 : [ (64, 3), (128, 4), (256, 6),  (512, 3) ],           # ResNet50
                101: [ (64, 3), (128, 4), (256, 23), (512, 3) ],           # ResNet101
                152: [ (64, 3), (128, 8), (256, 36), (512, 3) ]            # ResNet152

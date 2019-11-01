6e34ba2dc6ea557d0dc68f17f3241200ba9573e0
Andrew Ferlitsch <aferlitsch@gmail.com>
2019-10-31 17:10:51 -0700

    add metaparameters to composable

--- zoo/mobilenet/mobilenet_v1_c.py
+++ zoo/mobilenet/mobilenet_v1_c.py
@@ -24,6 +24,12 @@ from tensorflow.keras.layers import DepthwiseConv2D, GlobalAveragePooling2D, Res
 
 class MobileNetV1(object):
     """ Construct a Mobile Convolution Neural Network """
+    # Meta-parameter: number of filters and number of blocks per group
+    groups = [ { 'n_filters': 128,  'n_blocks': 2 },
+               { 'n_filters': 256,  'n_blocks': 2 },
+               { 'n_filters': 512,  'n_blocks': 6 },
+               { 'n_filters': 1024, 'n_blocks': 2 } ]
+
     # Meta-parameter: width multiplier (0 .. 1) for reducing number of filters.
     alpha      = 1   
     # Meta-parameter: resolution multiplier (0 .. 1) for reducing input size
@@ -33,13 +39,16 @@ class MobileNetV1(object):
     init_weights='glorot_uniform'
     _model = None
 
-    def __init__(self, alpha=1, pho=1, dropout=0.5, input_shape=(224, 224, 3), n_classes=1000):
+    def __init__(self, alpha=1, pho=1, dropout=0.5, groups=None, input_shape=(224, 224, 3), n_classes=1000):
         """ Construct a Mobile Convolution Neural Network
             alpha      : width multipler
             pho        :
             input_shape: the input shape
             n_classes  : number of output classes
         """
+        if groups is None:
+             groups = self.groups
+
         if alpha < 0 or alpha > 1:
             raise Exception("MobileNet: alpha out of range")
         if pho < 0 or pho > 1:
@@ -50,13 +59,13 @@ class MobileNetV1(object):
         inputs = Input(shape=(int(input_shape[0] * pho), int(input_shape[1] * pho), 3))
 
         # The Stem Group
-        x = self.stem(inputs, alpha)    
+        x = self.stem(inputs, alpha=alpha)    
 
         # The Learner
-        x = self.learner(x, alpha)
+        x = self.learner(x, groups=groups, alpha=alpha)
 
-        # The classifier 
-        outputs = self.classifier(x, alpha, dropout, n_classes)
+        # The Classifier 
+        outputs = self.classifier(x, n_classes, alpha=alpha, dropout=dropout)
 
         # Instantiate the Model
         self._model = Model(inputs, outputs)
@@ -67,13 +76,15 @@ class MobileNetV1(object):
 
     @model.setter
     def model(self, _model):
-        self._model - _model
+        self._model = _model
     
-    def stem(self, inputs, alpha):
+    def stem(self, inputs, **metaparameters):
         """ Construct the Stem Group
             inputs : input tensor
             alpha  : width multiplier
         """
+        alpha = metaparameters['alpha']
+
         # Convolutional block
         x = ZeroPadding2D(padding=((0, 1), (0, 1)))(inputs)
         x = Conv2D(32 * alpha, (3, 3), strides=(2, 2), padding='valid', use_bias=False, kernel_initializer=self.init_weights)(x)
@@ -81,51 +92,51 @@ class MobileNetV1(object):
         x = ReLU(6.0)(x)
 
         # Depthwise Separable Convolution Block
-        x = MobileNetV1.depthwise_block(x, 64, alpha, (1, 1))
+        x = MobileNetV1.depthwise_block(x, (1, 1), n_filters=64, alpha=alpha)
         return x
     
-    def learner(self, x, alpha):
+    def learner(self, x, **metaparameters):
         """ Construct the Learner
             x      : input to the learner
             alpha  : width multiplier
+            groups : number of filters and blocks per group
         """
-        # First Depthwise Separable Convolution Group
-        x = MobileNetV1.group(x, 128, 2, alpha)
+        alpha  = metaparameters['alpha']
+        groups = metaparameters['groups']
 
-        # Second Depthwise Separable Convolution Group
-        x = MobileNetV1.group(x, 256, 2, alpha)
+        # Add Depthwise Separable Convolution Group
+        for group in groups:
+            x = MobileNetV1.group(x, **group, alpha=alpha)
 
-        # Third Depthwise Separable Convolution Group
-        x = MobileNetV1.group(x, 512, 6, alpha)
-
-        # Fourth Depthwise Separable Convolution Group
-        x = MobileNetV1.group(x, 1024, 2, alpha)
         return x
 
     @staticmethod
-    def group(x, n_filters, n_blocks, alpha, init_weights=None):
+    def group(x, init_weights=None, **metaparameters):
         """ Construct a Depthwise Separable Convolution Group
             x         : input to the group
-            n_filters : number of filters
             n_blocks  : number of blocks in the group
-            alpha     : width multiplier
         """   
+        n_blocks  = metaparameters['n_blocks']
+
         # In first block, the depthwise convolution is strided - feature map size reduction
-        x = MobileNetV1.depthwise_block(x, n_filters, alpha, strides=(2, 2), init_weights=init_weights)
+        x = MobileNetV1.depthwise_block(x, strides=(2, 2), init_weights=init_weights, **metaparameters)
     
         # Remaining blocks
         for _ in range(n_blocks - 1):
-            x = MobileNetV1.depthwise_block(x, n_filters, alpha, strides=(1, 1), init_weights=init_weights)
+            x = MobileNetV1.depthwise_block(x, strides=(1, 1), init_weights=init_weights, **metaparameters)
         return x
 
     @staticmethod
-    def depthwise_block(x, n_filters, alpha, strides, init_weights=None):
+    def depthwise_block(x, strides, init_weights=None, **metaparameters):
         """ Construct a Depthwise Separable Convolution block
             x         : input to the block
             n_filters : number of filters
             alpha     : width multiplier
             strides   : strides
         """
+        n_filters = metaparameters['n_filters']
+        alpha     = metaparameters['alpha']
+
         if init_weights is None:
             init_weights = MobileNetV1.init_weights
             
@@ -150,13 +161,16 @@ class MobileNetV1(object):
         x = ReLU(6.0)(x)
         return x
 
-    def classifier(self, x, alpha, dropout, n_classes):
+    def classifier(self, x, n_classes, **metaparameters):
         """ Construct the classifier group
             x         : input to the classifier
             alpha     : width multiplier
             dropout   : dropout percentage
             n_classes : number of output classes
         """
+        alpha   = metaparameters['alpha']
+        dropout = metaparameters['dropout']
+
         # Flatten the feature maps into 1D feature maps (?, N)
         x = GlobalAveragePooling2D()(x)
 

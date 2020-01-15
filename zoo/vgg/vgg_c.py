ba5f1e8e25bd0427521a68cb40cd9f6f6e833c3f
Andrew Ferlitsch <aferlitsch@gmail.com>
2020-01-14 16:35:42 -0800

    add evaluate() to example

--- zoo/vgg/vgg_c.py
+++ zoo/vgg/vgg_c.py
@@ -160,3 +160,4 @@ def example():
     x_train = (x_train / 255.0).astype(np.float32)
 
     vgg.model.fit(x_train, y_train, epochs=10, batch_size=32, verbose=1)
+    vgg.model.evaluate(x_test, y_test)

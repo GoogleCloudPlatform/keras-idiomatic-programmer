ef23a32fd596fb08ac85cf93be9e6c8313257c3e
Andrew Ferlitsch <aferlitsch@gmail.com>
2020-01-27 15:43:45 -0800

    corrections to composable version of CIFAR-10

--- zoo/resnet/resnet_cifar10_v1_c.py
+++ zoo/resnet/resnet_cifar10_v1_c.py
@@ -28,19 +28,27 @@ from models_c import Composable
 class ResNetCifarV1(Composable):
     """ Residual Convolutional Neural Network V1
     """
-    #-------------------
-    # Model      | n   |
-    # ResNet20   | 2   |
-    # ResNet56   | 6   |
-    # ResNet110  | 12  |
-    # ResNet164  | 18  |
-    # ResNet1001 | 111 |
+    groups = { 20 : [ { 'n_filters': 16, 'n_blocks': 3},
+                      { 'n_filters': 32, 'n_blocks': 3},
+                      { 'n_filters': 64, 'n_blocks': 3}],
+               32 : [ { 'n_filters': 16, 'n_blocks': 5},
+                      { 'n_filters': 32, 'n_blocks': 5},
+                      { 'n_filters': 64, 'n_blocks': 5}],
+               44 : [ { 'n_filters': 16, 'n_blocks': 7},
+                      { 'n_filters': 32, 'n_blocks': 7},
+                      { 'n_filters': 64, 'n_blocks': 7}],
+               56 : [ { 'n_filters': 16, 'n_blocks': 9},
+                      { 'n_filters': 32, 'n_blocks': 9},
+                      { 'n_filters': 64, 'n_blocks': 9}],
+               110: [ { 'n_filters': 16, 'n_blocks': 18},
+                      { 'n_filters': 32, 'n_blocks': 18},
+                      { 'n_filters': 64, 'n_blocks': 18}] }
 
-    def __init__(self, n=2, m=2,
+    def __init__(self, n_layers,
                  input_shape=(32, 32, 3), n_classes=10, include_top=True,
                  reg=l2(0.001), relu=None, init_weights='he_normal', bias=False):
         """ Construct a Residual Convolutional Neural Network V1
-            n           :
+            n_layers    : number of layers
             input_shape : input shape
             n_classes   : number of output classes
             include_top : whether to include classifier
@@ -52,11 +60,13 @@ class ResNetCifarV1(Composable):
         # Configure the base (super) class
         super().__init__(reg=reg, relu=relu, init_weights=init_weights, bias=bias)
 
-        if n not in [2, 6, 12, 18, 111]:
-            raise ""
-
-        depth =  n * 9 + 2
-        n_blocks = ((depth - 2) // 9) - 1
+        # depth
+        if isinstance(n_layers, int):
+            if n_layers not in [20, 32, 44, 56, 110]:
+                raise Exception("ResNet CIFAR: invalid value for n_layers")
+            groups = list(self.groups[n_layers])
+        else:
+            groups = n_layers
 
         # The input tensor
         inputs = Input(input_shape)
@@ -65,7 +75,7 @@ class ResNetCifarV1(Composable):
         x = self.stem(inputs)
 
         # The learner
-        outputs = self.learner(x, n_blocks=n_blocks)
+        outputs = self.learner(x, groups=groups)
 
         # The classifier
         if include_top:
@@ -85,63 +95,69 @@ class ResNetCifarV1(Composable):
     
     def learner(self, x, **metaparameters):
         """ Construct the Learner
-            x          : input to the learner
-            n_blocks   : number of blocks in a group
+            x        : input to the learner
+            groups   : filter/blocks per group
         """
-        n_blocks = metaparameters['n_blocks']
+        groups = metaparameters['groups']
 
-        # First Residual Block Group of 16 filters (Stage 1)
-        # Quadruple (4X) the size of filters to fit the next Residual Group
-        x = self.group(x, 16, n_blocks, strides=(1, 1), m=4)
+        # first group uses strides=1 for first convolution
+        x = self.group(x, **groups.pop(0), proj=False)
 
-        # Second Residual Block Group of 64 filters (Stage 2)
-        # Double (2X) the size of filters and reduce feature maps by 75% (strides=2) to fit the next Residual Group
-        x = self.group(x, 64, n_blocks, m=2)
+        # remaining greoups
+        for group in groups:
+            x = self.group(x, **group)
 
-        # Third Residual Block Group of 64 filters (Stage 3)
-        # Double (2X) the size of filters and reduce feature maps by 75% (strides=2) to fit the next Residual Group
-        x = self.group(x, 128, n_blocks, m=2)
         return x
 
-    def group(self, x, n_filters, n_blocks, strides=(2, 2), m=2):
+    def group(self, x, n_filters, n_blocks, proj=True):
         """ Construct a Residual Group
             x         : input into the group
             n_filters : number of filters for the group
             n_blocks  : number of residual blocks with identity link
-            strides   : whether the projection block is a strided convolution
-            m         : multiplier for the number of filters out
+            proj      : whether first block is a projection block
         """
-        # Double the size of filters to fit the first Residual Group
-        x = self.projection_block(x, n_filters, strides=strides, m=m)
+        if proj:
+            x = self.projection_block(x, n_filters)
 
         # Identity residual blocks
-        for _ in range(n_blocks):
-            x = self.identity_block(x, n_filters, m=m)
+        for _ in range(n_blocks-1):
+            x = self.identity_block(x, n_filters)
         return x
     
-    def identity_block(self, x, n_filters, m=2):
+    def identity_block(self, x, n_filters):
         """ Construct a Bottleneck Residual Block of Convolutions with Identity Shortcut
             x        : input into the block
             n_filters: number of filters
-            m        : multiplier for filters out
         """
         # Save input vector (feature maps) for the identity link
         shortcut = x
     
-        ## Construct the 1x1, 3x3, 1x1 residual block (fig 3c)
-
-        # Dimensionality reduction
-        x = self.Conv2D(x, n_filters, (1, 1), strides=(1, 1))
+        x = self.Conv2D(x, n_filters, (3, 3), strides=(1, 1), padding='same')
         x = self.BatchNormalization(x)
         x = self.ReLU(x)
 
-        # Bottleneck layer
-        x = self.Conv2D(x, n_filters, (3, 3), strides=(1, 1), padding="same")
+        x = self.Conv2D(x, n_filters, (3, 3), strides=(1, 1), padding='same')
+        x = self.BatchNormalization(x)
+
+        # Add the identity link (input) to the output of the residual block
+        x = Add()([x, shortcut])
+        x = self.ReLU(x)
+        return x
+    
+    def projection_block(self, x, n_filters):
+        """ Construct a Bottleneck Residual Block of Convolutions with Identity Shortcut
+            x        : input into the block
+            n_filters: number of filters
+        """
+        # Save input vector (feature maps) for the identity link
+        shortcut = x
+        shortcut = self.Conv2D(shortcut, n_filters, (1, 1), strides=(2, 2), padding='same')
+    
+        x = self.Conv2D(x, n_filters, (3, 3), strides=(2, 2), padding='same')
         x = self.BatchNormalization(x)
         x = self.ReLU(x)
 
-        # Dimensionality restoration - increase the number of output filters by 2X or 4X
-        x = self.Conv2D(x, n_filters * m, (1, 1), strides=(1, 1))
+        x = self.Conv2D(x, n_filters, (3, 3), strides=(1, 1), padding='same')
         x = self.BatchNormalization(x)
 
         # Add the identity link (input) to the output of the residual block
@@ -149,39 +165,6 @@ class ResNetCifarV1(Composable):
         x = self.ReLU(x)
         return x
 
-    def projection_block(self, x, n_filters, strides=(2,2), m=2):
-        """ Construct Bottleneck Residual Block with Projection Shortcut
-            Increase the number of filters by 2X (or 4X on first stage)
-            x        : input into the block
-            n_filters: number of filters
-            strides  : whether the first convolution is strided
-            m        : multiplier for number of filters out
-        """
-        # Construct the projection shortcut
-        # Increase filters by 2X (or 4X) to match shape when added to output of block
-        shortcut = self.Conv2D(x, n_filters * m, (1, 1), strides=strides)
-
-        ## Construct the 1x1, 3x3, 1x1 convolution block
-
-        # Dimensionality reduction
-        x = self.Conv2D(x, n_filters, (1, 1), strides=(1,1))
-        x = self.BatchNormalization(x)
-        x = self.ReLU(x)
-    
-        # Bottleneck layer - feature pooling when strides=(2, 2)
-        x = self.Conv2D(x, n_filters, (3, 3), strides=strides, padding='same')
-        x = self.BatchNormalization(x)
-        x = self.ReLU(x)  
-    
-        # Dimensionality restoration - increase the number of filters by 2X (or 4X)
-        x = self.Conv2D(x, n_filters * m, (1, 1), strides=(1, 1))
-        x = self.BatchNormalization(x)
-
-        # Add the projection shortcut to the output of the residual block
-        x = Add()([shortcut, x])
-        x = self.ReLU(x)
-        return x
-    
     def classifier(self, x, n_classes):
         ''' Construct the Classifier
             x         : input into the classifier
@@ -200,5 +183,4 @@ class ResNetCifarV1(Composable):
         return outputs
 
 # Example
-cifar = ResNetCifarV1(2)
-# wcifar.model.summary()
+# cifar = ResNetCifarV1(20)
